f299c2a1195b6a91c758d3e92a053cabc77d052c
Tarsila Bianca Domningues de Carvalho <tarsila@carvalho>
2017-03-24 20:44:10 -0300

    Novas aulas

---
+++ AtividadeExtra/AtividadeExtra/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AtividadeExtra
+{
+    partial class frmNumero
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAcerto = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblAcerto
+            // 
+            this.lblAcerto.AutoSize = true;
+            this.lblAcerto.Location = new System.Drawing.Point(13, 13);
+            this.lblAcerto.Name = "lblAcerto";
+            this.lblAcerto.Size = new System.Drawing.Size(46, 13);
+            this.lblAcerto.TabIndex = 0;
+            this.lblAcerto.Text = "Acertos:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(56, 13);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "0";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(16, 61);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(32, 24);
+            this.lblNum1.TabIndex = 2;
+            this.lblNum1.Text = "11";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(152, 64);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(32, 24);
+            this.lblNum2.TabIndex = 3;
+            this.lblNum2.Text = "11";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(73, 61);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(57, 29);
+            this.txtNum.TabIndex = 4;
+            // 
+            // frmNumero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 116);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblAcerto);
+            this.Name = "frmNumero";
+            this.Text = "Número do meio";
+            this.Load += new System.EventHandler(this.frmNumero_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAcerto;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.TextBox txtNum;
+    }
+}
+
---
+++ AtividadeExtra/AtividadeExtra/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeExtra
+{
+    public partial class frmNumero : Form
+    {
+        public frmNumero()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNumero_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
